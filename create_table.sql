0c70fe66f56761a3f66ebb98b9ca1b2329df6ab4
dynolinuxtech01 <chocopluquthuq@gmail.com>
2026-06-29 17:42:42 +0700

    Add files via upload

---
+++ create_table.sql
@@ -0,0 +1,17 @@
+==
+=========================================
+==
+--   PROJECT 6 : ANALISIS TOP CUSTOMER
+--
+--      Author : Purwa Candra
+--      Database : PostgreSQL
+--      Tools : Grafana
+--
+==========================================
+==
+
+CREATE TABLE top_costumer
+(id INTEGER PRIMARY KEY,tanggal DATE,customer VARCHAR(100),
+ kota VARCHAR(100),total_belanja NUMERIC);
+
+
